81eefd20e68bc1e389b90881ebdd768937d74bbe
Anastasiya <test@mail.test>
2022-11-01 23:13:09 +0200

    task implementation

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
+        String[] input = scanner.nextLine().trim().split("\\s");
+        int sum = 0;
 
+        for (int i = 0; i < input.length - 1; i++) {
+            sum += Integer.parseInt(input[i]);
+        }
+
+        int average = sum / (input.length - 1);
+        System.out.println(average);
     }
-
 }
